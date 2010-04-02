65b4034b0590007931e5117efbc91862138df19e
mirek <miron_w@o2.pl>
2010-04-03 01:23:08 +0200

    Finish chapter 4

--- spec/integration/layout_links_spec.rb
+++ spec/integration/layout_links_spec.rb
@@ -27,4 +27,18 @@ describe "LayoutLinks" do
     response.should render_template('users/new')
   end
 
+  it "Should have the right links on pages" do
+    visit root_path
+    click_link "About"
+    response.should render_template('pages/about')
+    click_link "Contact"
+    response.should render_template('pages/contact')
+    click_link "Home"
+    response.should render_template('pages/home')
+    click_link "Help"
+    response.should render_template('pages/help')
+    click_link "Sign up"
+    response.should render_template('users/new')
+  end
+
 end
