e810646893393c09864423179dd3b4a43142f5c9
mirek <miron_w@o2.pl>
2010-04-06 20:55:13 +0200

    bolo

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,19 +5,19 @@ describe UsersController do
   #Delete these examples and add some real ones
     
   it "should use UsersController" do
-      controller.should be_an_instance_of(UsersController)
-    end
+    controller.should be_an_instance_of(UsersController)
+  end
 
   describe "GET 'new'" do
     it "should be successful" do
-      get 'new'
+      get :new
       response.should be_success
     end
   end
 
   describe "GET 'new'" do
     it "should be successful" do
-      get 'new'
+      get :new
       response.should have_tag("title", /Sign up/)
     end
   end
@@ -43,7 +43,43 @@ describe UsersController do
       get :show, :id => @user
       response.should have_tag("h2", /#{@user.name}/)
     end
-
   end
 
+  describe "Post 'create" do
+    describe "failure" do
+      before(:each) do
+        @attr = {:name => "", :email => "", :password =>"",
+          :password_confirmation => ""}
+        @user = Factory.build(:user, @attr)
+        User.stub!(:new).and_return(@user)
+        @user.should_receive(:save).and_return(false)
+      end
+
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_tag("title", /Sign up/i)
+      end
+      
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+
+    describe "success" do
+       before(:each) do
+        @attr = { :name => "New User", :email => "user@example.com",
+                  :password => "foobar", :password_confirmation => "foobar" }
+        @user = Factory(:user, @attr)
+        User.stub!(:new).and_return(@user)
+        @user.should_receive(:save).and_return(true)
+      end
+
+    it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(@user))
+      end
+    end
+
+  end
 end
